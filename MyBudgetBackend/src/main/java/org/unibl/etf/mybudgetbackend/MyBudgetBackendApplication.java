e7fda40d75e36fe87615430ab155f7254d8d8687
Nemanja Milošević <nemanjamilosevic117@gmail.com>
2024-08-10 10:29:13 +0200

    backend project initialization

---
+++ MyBudgetBackend/src/main/java/org/unibl/etf/mybudgetbackend/MyBudgetBackendApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.mybudgetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBudgetBackendApplication.class, args);
+    }
+
+}
---
+++ MyBudgetBackend/src/test/java/org/unibl/etf/mybudgetbackend/MyBudgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.mybudgetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
